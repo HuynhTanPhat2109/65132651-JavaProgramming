0e5caab136146496b9c02817547646427129b4af
HuynhTanPhat2109 <phathuynhcr091105@gmail.com>
2026-01-21 20:49:26 +0700

    Thêm Lab1_Bai1_DTB

---
+++ Lab1_HuynhTanPhat/src/Lab1/Lab1_Bai1_DTB.java
@@ -0,0 +1,20 @@
+package Lab1;
+import java.util.Iterator;
+import java.util.Scanner;
+public class Lab1_Bai1_DTB {
+	public static void main(String[] args) {
+		Scanner scanner =  new Scanner(System.in);
+		
+		System.out.print("Nhập vào Họ và Tên sinh viên: ");
+		String hoTen = scanner.nextLine();
+		
+		System.out.print("Nhập vào điểm trung bình: ");
+		Double diemTB = Double.parseDouble(scanner.nextLine());
+		
+		System.out.print("\n===Thông tin sinh viên ===\n");
+		System.out.printf("\nHọ và tên: %s", hoTen);
+		System.out.printf("\nĐiểm trung bình: %.2f", diemTB);
+		
+		scanner.close();
+	}
+}
